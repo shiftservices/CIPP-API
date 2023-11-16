454124f4c1e46a90fc26bc0bf50e0b8f6f975bd9
Ivan Filippov <ivan.v.f@gmail.com>
2023-11-15 17:00:18 -0700

    Revert "Make AddTransportRule debugging great again"
    This reverts commit 3f0cb19925b6d029f9cfb87c2df51fadb4996720.

--- AddTransportRule/run.ps1
+++ AddTransportRule/run.ps1
@@ -27,16 +27,7 @@ $Result = foreach ($Tenantfilter in $tenants) {
         Write-LogMessage -API $APINAME -tenant $tenantfilter -message "Created transport rule for $($tenantfilter)" -sev Debug
     }
     catch {
-        $formatstring = "{0} : {1}`n{2}`n" +
-                "    + CategoryInfo          : {3}`n" +
-                "    + FullyQualifiedErrorId : {4}`n"
-        $fields = $_.InvocationInfo.MyCommand.Name,
-                  $_.ErrorDetails.Message,
-                  $_.InvocationInfo.PositionMessage,
-                  $_.CategoryInfo.ToString(),
-                  $_.FullyQualifiedErrorId
-        
-        $formatstring -f $fields
+        $_
         "Could not create transport rule for $($tenantfilter): $($_.Exception.message)"
     }
 }
